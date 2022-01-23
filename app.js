af4003ff2dcf092839e87f8a7c4f75d029c33da6
Thiago <thiagowirzdemoura@gmail.com>
2022-01-23 03:12:17 -0300

    Request Post /sign-up

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import express from "express"
+import cors from "cors"
 
 const app = express()
+app.use(express.json())
+app.use(cors())
 
 
 let users = []
 
 let tweets = []
 
-app.listen(5000)
+app.post("/sign-up", (req, res) =>{
+    users.push(req.body);
+    console.log(users)
+    res.send("Ok");
+})
+
+app.listen(5000, () =>{
+    console.log("Rodando em http://localhost:5000")
+})
