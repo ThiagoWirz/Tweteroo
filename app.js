daad5260ae885d4a2d984fb64b24661a3dbda529
Thiago <thiagowirzdemoura@gmail.com>
2022-01-23 05:40:10 -0300

    Get tweets Request

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
+let users = [];
+let tweets = [];
+let currentUser;
 
-let users = []
+app.post("/sign-up", (req, res) => {
+  currentUser = req.body
+  users.push(req.body);
+  res.send("Ok");
+});
 
-let tweets = []
+app.post("/tweets", (req, res) => {
+  let tweet = { username : req.body.username,
+    avatar: currentUser.avatar,
+    tweet: req.body.tweet
+  }
+  tweets.splice(0, 0, tweet);
+  res.send("Ok");
+});
 
-app.post("/sign-up", (req, res) =>{
-    users.push(req.body);
-    res.send("Ok");
+app.get("/tweets", (req, res) =>{
+  let lastTweets = []
+  if(tweets.length < 10){
+    for(let tweet of tweets){
+      lastTweets.push(tweet)
+    }
+  }
+  else{
+    for(let i = 0; i< 10; i++){
+      lastTweets.push(tweets[i])
+    }
+  }
+
+  res.send(lastTweets)
 })
 
-app.post("/tweets", (req, res) =>{
-    tweets.push(req.body);
-    res.send("Ok");
-})
 
-app.listen(5000, () =>{
-    console.log("Rodando em http://localhost:5000")
-})
+app.listen(5000, () => {
+  console.log("Rodando em http://localhost:5000");
+});
