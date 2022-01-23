25e37c420b1eb422e90d7fda053afbbc8d8bfa92
Thiago <thiagowirzdemoura@gmail.com>
2022-01-23 06:02:41 -0300

    Bad Request error code 400, in sign-up and tweets post request

--- app.js
+++ app.js
@@ -10,36 +10,48 @@ let tweets = [];
 let currentUser;
 
 app.post("/sign-up", (req, res) => {
-  currentUser = req.body
-  users.push(req.body);
-  res.send("Ok");
+  if (
+    !req.body.username ||
+    req.body.username === "" ||
+    !req.body.avatar ||
+    req.body.avatar === ""
+  ) {
+    res.status(400).send("Todos os campos são obrigatórios!");
+  } else {
+    currentUser = req.body;
+    users.push(req.body);
+    res.send("Ok");
+  }
 });
 
 app.post("/tweets", (req, res) => {
-  let tweet = { username : req.body.username,
+  if(!req.body.username || req.body.username === "" || !req.body.tweet || req.body.tweet === ""){
+    res.status(400).send("Todos os campos são obrigatórios!");
+  } else{
+    let tweet = {
+    username: req.body.username,
     avatar: currentUser.avatar,
-    tweet: req.body.tweet
-  }
+    tweet: req.body.tweet,
+  };
   tweets.splice(0, 0, tweet);
   res.send("Ok");
+  }
 });
 
-app.get("/tweets", (req, res) =>{
-  let lastTweets = []
-  if(tweets.length < 10){
-    for(let tweet of tweets){
-      lastTweets.push(tweet)
+app.get("/tweets", (req, res) => {
+  let lastTweets = [];
+  if (tweets.length < 10) {
+    for (let tweet of tweets) {
+      lastTweets.push(tweet);
     }
-  }
-  else{
-    for(let i = 0; i< 10; i++){
-      lastTweets.push(tweets[i])
+  } else {
+    for (let i = 0; i < 10; i++) {
+      lastTweets.push(tweets[i]);
     }
   }
 
-  res.send(lastTweets)
-})
-
+  res.send(lastTweets);
+});
 
 app.listen(5000, () => {
   console.log("Rodando em http://localhost:5000");
